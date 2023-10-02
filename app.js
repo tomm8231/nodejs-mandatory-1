08adbe22471fa9137d33b73c3cb41417cf183d0f
Tommyk <tomm8231@stud.kea.dk>
2023-10-02 21:11:15 +0200

    improved project structure, more  pages and endpoints added

--- app.js
+++ app.js
@@ -6,11 +6,27 @@ const app = express()
 app.use(express.static("public"))
 
 app.get("/", (req, res) => {
-    res.sendFile(path.resolve("./public/frontpage.html"))
+    res.sendFile(path.resolve("./public/frontpage/frontpage.html"))
 })
 
-app.get("/topic-1", (req, res) => {
-    res.sendFile(path.resolve("./public/topic1.html"))
+app.get("/express", (req, res) => {
+    res.sendFile(path.resolve("./public/express/express.html"))
+})
+
+app.get("/nodejs", (req, res) => {
+    res.sendFile(path.resolve("./public/nodejs/nodejs.html"))
+})
+
+app.get("/nodemon", (req, res) => {
+    res.sendFile(path.resolve("./public/nodemon/nodemon.html"))
+})
+
+app.get("/client-server-model", (req, res) => {
+    res.sendFile(path.resolve("./public/client-server-model/client-server-model.html"))
+})
+
+app.get("/type-coercion", (req, res) => {
+    res.sendFile(path.resolve("./public/type-coercion/type-coercion.html"))
 })
 
 app.listen(8080, () => {
